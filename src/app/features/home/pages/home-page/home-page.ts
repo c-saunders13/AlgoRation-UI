d47ce3b40f013f0d847bc98221cf95e7210912a2
Carl Saunders <41913276+c-saunders13@users.noreply.github.com>
2026-03-29 19:29:43 +0200

    Clear calculation data upon data state restore

--- src/app/features/home/pages/home-page/home-page.ts
+++ src/app/features/home/pages/home-page/home-page.ts
@@ -60,6 +60,7 @@ export class HomePageComponent {
   protected restoreData(): void {
     this.loadingData.set(true);
     this.errorMessage.set(null);
+    this.calculationResult.set(null);
 
     this.recipeService
       .reset()
